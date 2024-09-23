5b1aaf97065fcb4fb637fb0cdb7a3b54c5ff6e41
vassidiqi <vassidiqi@gmail.com>
2024-09-23 08:23:27 +0700

    fix background; decorate main intro text

--- src/sections/main-intro/index.tsx
+++ src/sections/main-intro/index.tsx
@@ -2,8 +2,14 @@ import './main-intro.css'
 
 export default function MainIntro() {
   return (
-    <div className='main-intro'>
-
+    <div className='main-intro h-screen w-screen'>
+      <div className='bg-white/50 flex flex-col items-center justify-center p-3 w-full h-full z-50'>
+        <h1 className='main-intro-title text-6xl text-center'>"Hantaran" specially made for you</h1>
+        <br />
+        <br />
+        <br />
+        <h6 className=''>by HANTARANNYA HUMAIRA</h6>
+      </div>
     </div>
   )
 }
