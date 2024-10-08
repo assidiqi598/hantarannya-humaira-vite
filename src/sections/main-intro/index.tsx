afc1ed33f45a4fff6780894eb958f4565cd8a980
vassidiqi <vassidiqi@gmail.com>
2024-10-08 20:36:46 +0700

    change hantaran hidden; adjust size for mobile

--- src/sections/main-intro/index.tsx
+++ src/sections/main-intro/index.tsx
@@ -8,7 +8,7 @@ export default function MainIntro() {
   };
 
   return (
-    <section className="relative flex flex-col items-center justify-center h-screen w-screen bg-yellow-200 lg:w-full lg:max-h-screen">
+    <section className="relative flex flex-col items-center justify-center h-svh w-svw bg-yellow-200 lg:w-full lg:max-h-screen">
       <img
         src="assets/main-intro-bg.svg"
         alt="hantaran-seserahan-main-intro-bg"
@@ -22,7 +22,7 @@ export default function MainIntro() {
         loading="lazy"
       />
       <div className="flex flex-col items-center justify-end p-3 w-full lg:w-1/2 lg:px-0 absolute left-0 right-0 mx-auto intro-div">
-        <h1 className="main-intro-title text-6xl text-center">
+        <h1 className="main-intro-title text-5xl lg:text-6xl text-center">
           "Hantaran" specially made for you
         </h1>
         <p className="text-pink-600 mb-5">by HANTARANNYA HUMAIRA</p>
